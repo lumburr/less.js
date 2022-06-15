b6c9a06f3e7067b7845914456a89ce9434d6d8aa
lumburr <flyfish.zy@foxmail.com>
2022-06-15 20:46:08 +0800

    fix: embedded js keep quotes inside a list

--- packages/less/src/less/tree/javascript.js
+++ packages/less/src/less/tree/javascript.js
@@ -2,6 +2,7 @@ import JsEvalNode from './js-eval-node';
 import Dimension from './dimension';
 import Quoted from './quoted';
 import Anonymous from './anonymous';
+import Expression from './expression';
 
 const JavaScript = function(string, escaped, index, currentFileInfo) {
     this.escaped = escaped;
@@ -22,10 +23,37 @@ JavaScript.prototype = Object.assign(new JsEvalNode(), {
         } else if (type === 'string') {
             return new Quoted(`"${result}"`, result, this.escaped, this._index);
         } else if (Array.isArray(result)) {
-            return new Anonymous(result.join(', '));
+            return new Expression(
+                this.evalArray(result, this.escaped, this._index).map(function(e) {
+                    return e;
+                }),
+                true
+            );
         } else {
             return new Anonymous(result);
         }
+    },
+    evalArray(result, escaped, _index) {
+        let resultArr = []
+        for (let i = 0; i < result.length; i++) {
+            const type = typeof result[i];
+            if (type === 'number' && !isNaN(result[i])) {
+                resultArr.push(new Dimension(result[i]));
+                resultArr.push(new Anonymous(', ',_index));
+            } else if (type === 'string') {
+                resultArr.push(new Quoted(`"${result[i]}"`, result[i], escaped, _index));
+                resultArr.push(new Anonymous(', ',_index));
+            } else if (Array.isArray(result[i])) {
+                resultArr.push(new Anonymous(result[i].join(', ')));
+                resultArr.push(new Anonymous(', '),_index);
+            } else {
+                resultArr.push(new Anonymous(result[i]));
+                resultArr.push(new Anonymous(', '),_index);
+            }
+        }
+        resultArr.pop()
+        return resultArr
+        
     }
 });
 
